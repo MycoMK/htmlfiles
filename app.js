3e090337e8d1c35cd290c1a1bd3ebf464c407c3e
Myco <kmichael831@gmail.com>
2023-01-05 21:18:00 -0800

    edits-api

--- app.js
+++ app.js
@@ -1,5 +1,40 @@
-fetch('http://example.com/movies.json')
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+// fetch('http://example.com/movies.json'{
+//   method: 'post',
+//   headers:{
+//     'Content-Type':'apploication/json'
+//   },
+//   body: JSON.stringify({
+//     name: 'user 1'
+//   })
+// })
+//   .then(response => {
+//     response.json()})
+//   .then(data => console.log(data));
+ 
 
+  fetch('http://example.com/movies.json',{
+    method: 'post',
+    headers:{
+      'content-type':'application/json'
+    },
+    body: JSON.stringify({
+      name:'User1'
+    })
+  })
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch('error' = console.log('ERROR'));
+
+  fetch('http://example.com/movies.json',{
+    method: "post",
+    headers:{
+      'content-type':'application/json'
+  },
+    body:JSON.stringify({
+      body: 'user1'
+    })
+  })
   
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch(error => console.log('ERROR'))
